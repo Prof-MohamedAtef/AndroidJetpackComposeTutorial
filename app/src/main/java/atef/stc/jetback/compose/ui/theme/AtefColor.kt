e9b787e32b9af51f3ac6cdb75cd1df3ce752632d
MohamedAtef <maabdallah.c@stc.com.sa>
2023-06-09 18:59:12 +0300

    initializig App theme

---
+++ app/src/main/java/atef/stc/jetback/compose/ui/theme/AtefColor.kt
@@ -0,0 +1,127 @@
+package ae.etisalat.smiles.basecompose.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Primary = Color(0xFF75428e)
+val Secondary = Color(0xFF424c99)
+val Accent = Color(0xFF8754a1)
+val Disabled = Color(0xFFe9e9ec)
+
+//basic Text views colors
+val Default_Text = Color(0xFF131728)
+val Default_Text_Disabled = Color(0xFFa6a8b3)
+val Default_Text_On_Dark_BG = Color(0xFFFFFFFF)
+
+//Buttons Colors
+val Default_Button_BG = Primary
+val Default_Button_Disabled_BG = Disabled
+val Default_Button_Disabled_Text = Default_Text_Disabled
+
+val OUTLINE_BUTTON_TEXT_COLOR = Primary
+val OUTLINE_BUTTON_BORDER_COLOR = Primary
+
+//Checkbox and radio button colors
+val Default_Checkmark_Color = Color(0xFFFFFFFF)
+val Default_Checked_BG_Color = Accent
+
+val DIVIDER_LINE = Color(0x33000000)
+val DOB_SUBTITLE_COLOR = Color(0xFF6d6670)
+
+
+val Gray = Color(0xFF797d8d)
+val DividerColor = Color(0xFFa6a8b3)
+val GraySubTitleText = Color(0xFF797d8d)
+val TabSelectedColor = Primary //Color(0xFF75428e)
+val TabUnSelectedColor = Color(0xFF797d8d)
+val LightGray = Color(0xFFd2d4d9)
+val DarkGray = Color(0xFF4d5167)
+
+val HomeGradientStartColor = Primary
+val HomeGradientEndColor = Secondary
+
+val ToolbarLightColor = Color(0xFF1A1D1D)
+val ToolbarDarkColor = Color(0xFFFFFFFF)
+val LightBlueBg = Color(0xFFEFF0F9)
+val SearchBorderColor = Color(0xFFD2D4D9)
+
+val RESTAURANT_GRAY_COLOR = Color(0xff797d8d)
+val RESTAURANT_AMOUNT_COLOR = Color(0xff4d5167)
+
+val FilterChipsBorderColor = Color(0xFFa6a8b3)
+val FILTER_CHIPS_SELECTED_BORDER_COLOR = Color(0xFFd2d4d9)
+val FilterChipsBGColor = Color(0xFFeff0f9)
+val FILTER_COUNT_CIRCLE_COLOR = Color(0xFF424c99)
+val FILTER_SELECTED_COLOR = Color(0xFFffffff)
+
+val md_theme_light_surfaceTint = Color(0xFF6750A4)
+val md_theme_light_onErrorContainer = Color(0xFF410E0B)
+val md_theme_light_onError = Color(0xFFFFFFFF)
+val md_theme_light_errorContainer = Color(0xFFF9DEDC)
+val md_theme_light_onTertiaryContainer = Color(0xFF31111D)
+val md_theme_light_onTertiary = Color(0xFFFFFFFF)
+val md_theme_light_tertiaryContainer = Color(0xFFFFD8E4)
+val md_theme_light_tertiary = Color(0xFF7D5260)
+val md_theme_light_shadow = Color(0xFF000000)
+val md_theme_light_error = Color(0xFFB3261E)
+val md_theme_light_outline = Color(0xFF79747E)
+val md_theme_light_onBackground = Color(0xFF1C1B1F)
+val md_theme_light_background = Color(0xFFFFFBFE)
+val md_theme_light_inverseOnSurface = Color(0xFFF4EFF4)
+val md_theme_light_inverseSurface = Color(0xFF313033)
+val md_theme_light_onSurfaceVariant = Color(0xFF49454F)
+val md_theme_light_onSurface = Color(0xFF1C1B1F)
+val md_theme_light_surfaceVariant = Color(0xFFE7E0EC)
+val md_theme_light_surface = Color(0xFFFFFBFE)
+val md_theme_light_onSecondaryContainer = Color(0xFF1D192B)
+val md_theme_light_onSecondary = Color(0xFFFFFFFF)
+val md_theme_light_secondaryContainer = Color(0xFFE8DEF8)
+val md_theme_light_secondary = Color(0xFF625B71)
+val md_theme_light_inversePrimary = Color(0xFFD0BCFF)
+val md_theme_light_onPrimaryContainer = Color(0xFF21005D)
+val md_theme_light_onPrimary = Color(0xFFFFFFFF)
+val md_theme_light_primaryContainer = Color(0xFFEADDFF)
+val md_theme_light_primary = Color(0xFF6750A4)
+
+val md_theme_dark_surfaceTint = Color(0xFFD0BCFF)
+val md_theme_dark_onErrorContainer = Color(0xFFF2B8B5)
+val md_theme_dark_onError = Color(0xFF601410)
+val md_theme_dark_errorContainer = Color(0xFF8C1D18)
+val md_theme_dark_onTertiaryContainer = Color(0xFFFFD8E4)
+val md_theme_dark_onTertiary = Color(0xFF492532)
+val md_theme_dark_tertiaryContainer = Color(0xFF633B48)
+val md_theme_dark_tertiary = Color(0xFFEFB8C8)
+val md_theme_dark_shadow = Color(0xFF000000)
+val md_theme_dark_error = Color(0xFFF2B8B5)
+val md_theme_dark_outline = Color(0xFF938F99)
+val md_theme_dark_onBackground = Color(0xFFE6E1E5)
+val md_theme_dark_background = Color(0xFF1C1B1F)
+val md_theme_dark_inverseOnSurface = Color(0xFF313033)
+val md_theme_dark_inverseSurface = Color(0xFFE6E1E5)
+val md_theme_dark_onSurfaceVariant = Color(0xFFCAC4D0)
+val md_theme_dark_onSurface = Color(0xFFE6E1E5)
+val md_theme_dark_surfaceVariant = Color(0xFF49454F)
+val md_theme_dark_surface = Color(0xFF1C1B1F)
+val md_theme_dark_onSecondaryContainer = Color(0xFFE8DEF8)
+val md_theme_dark_onSecondary = Color(0xFF332D41)
+val md_theme_dark_secondaryContainer = Color(0xFF4A4458)
+val md_theme_dark_secondary = Color(0xFFCCC2DC)
+val md_theme_dark_inversePrimary = Color(0xFF6750A4)
+val md_theme_dark_onPrimaryContainer = Color(0xFFEADDFF)
+val md_theme_dark_onPrimary = Color(0xFF381E72)
+val md_theme_dark_primaryContainer = Color(0xFF4F378B)
+val md_theme_dark_primary = Color(0xFFD0BCFF)
+
+val seed = Color(0xFF6750A4)
+val bg_ghost_white = Color(0xFFf6f8ff)
+val bg_floral_white = Color(0xFFfaf7f2)
+val bg_bottom_sheet_color= Color(0x99131728)
+val bg_black_transparent = Color(0xD3070707)
+val bg_yellow_closing = Color(0xFFFBF1D3)
+val text_eclipse = Color(0xFF383838)
+val text_collections=Color(0xFF3d3341)
+val collection_border=Color(0xFFd2d4d9)
+val bg_collection_start=Color(0x00000000)
+val bg_collection_end=Color(0xd9000000)
+val bg_collection_center=Color(0x33000000)
+val bg_billing = Color(0xFFf5f7f9)
+val icon_shape = Color(0xFFBCBCBC)
