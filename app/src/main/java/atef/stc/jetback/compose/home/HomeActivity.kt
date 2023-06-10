a54a3c9f0f51c0b3b9b7a6f494546740d253a9d8
MohamedAtef <maabdallah.c@stc.com.sa>
2023-06-10 20:04:57 +0300

    Added navigation between SplashScreen, MainScreen, and DetailsScreen

--- app/src/main/java/atef/stc/jetback/compose/home/HomeActivity.kt
+++ app/src/main/java/atef/stc/jetback/compose/home/HomeActivity.kt
@@ -4,20 +4,22 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
-import atef.stc.jetback.compose.navigation.Navigation
+import atef.stc.jetback.compose.navigation.AppNavigation
 import atef.stc.jetback.compose.ui.theme.AndroidComposeTutorialTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Navigation()
+            Surface(color = Color(0xFF202020), modifier = Modifier.fillMaxSize()) {
+                AppNavigation()
+            }
         }
     }
 }
