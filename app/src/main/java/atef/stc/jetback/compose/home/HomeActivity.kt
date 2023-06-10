b680d908295e8bdfbc8bc318e88721d541b486ed
MohamedAtef <maabdallah.c@stc.com.sa>
2023-06-10 14:53:32 +0300

    Navigate to Details Screen

--- app/src/main/java/atef/stc/jetback/compose/home/HomeActivity.kt
+++ app/src/main/java/atef/stc/jetback/compose/home/HomeActivity.kt
@@ -1,4 +1,4 @@
-package atef.stc.jetback.compose
+package atef.stc.jetback.compose.home
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
@@ -10,18 +10,14 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import atef.stc.jetback.compose.navigation.Navigation
 import atef.stc.jetback.compose.ui.theme.AndroidComposeTutorialTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            AndroidComposeTutorialTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
-                }
-            }
+            Navigation()
         }
     }
 }
---
+++ app/src/main/java/atef/stc/jetback/compose/home/UpperListFragment.kt
@@ -0,0 +1,7 @@
+package atef.stc.jetback.compose.home
+
+import androidx.fragment.app.Fragment
+
+class UpperListFragment : Fragment() {
+
+}
