2b8d5fb32940462668777d9ddb421b2624094a2f
MohamedAtef <maabdallah.c@stc.com.sa>
2023-06-11 10:18:33 +0300

    return dummy categories list

--- app/src/main/java/atef/stc/jetback/compose/uistate/HomeCategoriesListState.kt
+++ app/src/main/java/atef/stc/jetback/compose/uistate/HomeCategoriesListState.kt
@@ -4,8 +4,22 @@ import atef.stc.jetback.compose.models.ItemCategoriesDetails
 import atef.stc.jetback.compose.models.SectionDetails
 
 data class HomeCategoriesListState(
-    val data: List<ItemCategoriesDetails> = emptyList(),
+    val data: List<ItemCategoriesDetails> = getDummyItemCategories(),
     override var sectionDetails: SectionDetails? = null,
     override var isLoading: Boolean = false,
     override var hasError: Boolean = false,
 ) : SectionBaseUIState(isLoading, hasError, sectionDetails)
+
+
+fun getDummyItemCategories() : List<ItemCategoriesDetails>{
+    return listOf<ItemCategoriesDetails>(
+        ItemCategoriesDetails(0, "Books", null, "https://img.freepik.com/free-vector/hand-drawn-flat-design-stack-books-illustration_23-2149341898.jpg?w=1380&t=st=1686467659~exp=1686468259~hmac=a745e2341fef3fd4dd7059a38be19ea0b344bd7afe30952ed3baf9160863614f", null, "#ffcccc",null, null, null, null, null,null),
+        ItemCategoriesDetails(0, "Books", null, "https://img.freepik.com/free-vector/hand-drawn-flat-design-stack-books-illustration_23-2149341898.jpg?w=1380&t=st=1686467659~exp=1686468259~hmac=a745e2341fef3fd4dd7059a38be19ea0b344bd7afe30952ed3baf9160863614f", null, "#ffcccc",null, null, null, null, null,null),
+        ItemCategoriesDetails(0, "Books", null, "https://img.freepik.com/free-vector/hand-drawn-flat-design-stack-books-illustration_23-2149341898.jpg?w=1380&t=st=1686467659~exp=1686468259~hmac=a745e2341fef3fd4dd7059a38be19ea0b344bd7afe30952ed3baf9160863614f", null, "#ffcccc",null, null, null, null, null,null),
+        ItemCategoriesDetails(0, "Books", null, "https://img.freepik.com/free-vector/hand-drawn-flat-design-stack-books-illustration_23-2149341898.jpg?w=1380&t=st=1686467659~exp=1686468259~hmac=a745e2341fef3fd4dd7059a38be19ea0b344bd7afe30952ed3baf9160863614f", null, "#ffcccc",null, null, null, null, null,null),
+        ItemCategoriesDetails(0, "Books", null, "https://img.freepik.com/free-vector/hand-drawn-flat-design-stack-books-illustration_23-2149341898.jpg?w=1380&t=st=1686467659~exp=1686468259~hmac=a745e2341fef3fd4dd7059a38be19ea0b344bd7afe30952ed3baf9160863614f", null, "#ffcccc",null, null, null, null, null,null),
+        ItemCategoriesDetails(0, "Books", null, "https://img.freepik.com/free-vector/hand-drawn-flat-design-stack-books-illustration_23-2149341898.jpg?w=1380&t=st=1686467659~exp=1686468259~hmac=a745e2341fef3fd4dd7059a38be19ea0b344bd7afe30952ed3baf9160863614f", null, "#ffcccc",null, null, null, null, null,null),
+        ItemCategoriesDetails(0, "Books", null, "https://img.freepik.com/free-vector/hand-drawn-flat-design-stack-books-illustration_23-2149341898.jpg?w=1380&t=st=1686467659~exp=1686468259~hmac=a745e2341fef3fd4dd7059a38be19ea0b344bd7afe30952ed3baf9160863614f", null, "#ffcccc",null, null, null, null, null,null),
+        ItemCategoriesDetails(0, "Books", null, "https://img.freepik.com/free-vector/hand-drawn-flat-design-stack-books-illustration_23-2149341898.jpg?w=1380&t=st=1686467659~exp=1686468259~hmac=a745e2341fef3fd4dd7059a38be19ea0b344bd7afe30952ed3baf9160863614f", null, "#ffcccc",null, null, null, null, null,null),
+    )
+}
